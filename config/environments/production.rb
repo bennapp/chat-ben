34f5f10fac93b2607784d9ac390a64ca96b54e4e
Ben Nappier <bennappier@gmail.com>
2016-03-12 22:27:19 -0800

    fix production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  public_file_server.enabled = true
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
