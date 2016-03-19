97fece81bd6d238763fb6144559055277eeb3028
Ben Nappier <bennappier@gmail.com>
2016-03-19 12:26:43 -0700

    fix deploy script

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,3 +6,4 @@ RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
 sudo service nginx restart
 sudo restart puma-manager
+sudo start puma app=/home/dev/workspace/convo
