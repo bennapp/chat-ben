56406c6c83789f326e1af7b13775860a0cab2cac
Ben Nappier <bennappier@gmail.com>
2016-05-03 19:05:52 -0700

    increase reaction size

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,7 +2,7 @@ class Reaction < ApplicationRecord
   default_scope { order("created_at DESC") }
   has_attached_file :video
   validates_attachment :video, content_type: { content_type: ["video/webm"] }
-  validates_attachment_size :video, :in => 0.kilobytes..1750.kilobytes
+  validates_attachment_size :video, :in => 0.kilobytes..2300.kilobytes
 
   belongs_to :user
   belongs_to :post
