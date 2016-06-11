005f35ff55a322d754b78a8ac5fa311c49f22746
Ben Nappier <bennappier@gmail.com>
2016-06-10 18:51:41 -0700

    sort bins correctly

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -71,7 +71,7 @@ class RoomChannel < ApplicationCable::Channel
   def advance_channel(data, options = {})
     room = Room.find_by_token(params[:room])
     current_bin = Bin.find(data['bin_id'])
-    bins = Bin.all.to_a
+    bins = Bin.all.sort_by { |bin| bin.id }.to_a
 
     current_bin_index = bins.index(current_bin)
 
