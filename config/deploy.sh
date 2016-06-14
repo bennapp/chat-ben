13b8a59c03d0658427a1f9af35a960cbed793118
Ben Nappier <bennappier@gmail.com>
2016-06-14 14:32:59 -0700

    remove sudo password from deploy script

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,6 +4,5 @@ git pull
 RAILS_ENV=production bundle install
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
-sudo service nginx restart
+sudo /home/dev/scripts/nginx-restart
 sudo restart puma-manager
-sudo start puma app=/home/dev/workspace/convo
