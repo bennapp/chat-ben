a58ac32fb8409451cbadda2c7035fbbd72b2940e
Ben Nappier <bennappier@gmail.com>
2016-06-14 14:40:00 -0700

    fix deploy script

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,5 +4,6 @@ git pull
 RAILS_ENV=production bundle install
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
-sudo /home/dev/scripts/nginx-restart
+sudo service nginx restart
 sudo restart puma-manager
+
