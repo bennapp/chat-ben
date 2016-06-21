d2569a2908b9b81ca5ef831c077d897499165732
Ben Nappier <bennappier@gmail.com>
2016-06-20 17:37:26 -0700

    actually delete participations

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,17 +1,8 @@
 class Participation < ActiveRecord::Base
-  # acts_as_paranoid
-  scope :without_deleted, -> { where(deleted_at: nil) }
-  scope :with_deleted, -> { where.not(deleted_at: nil) }
-  # end acts_as_paranoid
-
   belongs_to :room
   belongs_to :user
 
   after_commit :update_room_full
-
-  def destroy
-    update_attribute(:deleted_at, current_time_from_proper_timezone)
-  end
 
   def update_room_full
     room.update_attribute(:full, room.participations.count >= 2)
